efe0a7c65dc4feda0b8e948714f0a22bd8dfec44
miduki-gif <midzuki1935@gmail.com>
2023-09-24 03:09:52 +0900

    ''

---
+++ resources/views/blog/list.blade.php
@@ -0,0 +1,29 @@
+<!-- 
+①共通テンプレートlayout.blade.phpを作る
+②共通ヘッダーを作る
+③共通フッターを作る
+④共通テンプレを継承したリストを作る
+-->
+@extends('blog.layout')
+@section('title', 'ブログ一覧')
+@section('content')
+<div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <h2>ブログ記事一覧</h2>
+            <table class="table table-striped">
+                <tr>
+                    <th>記事番号</th>
+                    <th>日付</th>
+                    <th>タイトル</th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2020/06/30</td>
+                    <td>テスト</td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+        </div>
+        @endsection
