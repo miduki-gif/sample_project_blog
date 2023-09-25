916099820bade617288b1fa30c81330af8cfe1b7
miduki-gif <midzuki1935@gmail.com>
2023-09-25 16:14:45 +0900

    'blog_list_testdate'

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Blog;
 
 class BlogController extends Controller
 {
+    //①（M）Blog　Modelを呼び出す
+    //②(C) ControllerからBladeに渡す
+    //③（V）Bladeで表示する　
      /**
      * ブログ一覧を表示する
      * 
@@ -13,6 +17,8 @@ class BlogController extends Controller
      */
     public function showList()
     {
-        return view('blog.list');
+        $blogs = Blog::all();
+        
+        return view('blog.list',['blogs' =>$blogs ]);
     }
 }
